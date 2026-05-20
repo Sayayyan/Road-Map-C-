31218b250af3628f01c6b5b2081a312c40ebdaf0
Sayayyan <sofianoktoberbahtiar3@gmail.com>
2026-05-20 19:42:03 +0700

    first commit

---
+++ RM_HARI_2/RM_HARI_2/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("=================================================================");
+Console.WriteLine("           Keterangan Lulus Berdasarkan Nilai Ujian              ");
+Console.WriteLine("-----------------------------------------------------------------");
+Console.WriteLine();
+Console.Write("Masukkan Nilai Ujian = ");
+int nilaiujian = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if(nilaiujian >= 75)
+{
+    Console.WriteLine("Anda dinyatakan lulus!");
+}
+
+else
+{
+    Console.WriteLine("Anda dinyatakan tidak lulus!");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+//===============================================================================================================================================
+
+Console.WriteLine("=================================================================");
+Console.WriteLine("                Pembelian Buku Tulis (Rp7000)                    ");
+Console.WriteLine("-----------------------------------------------------------------");
+Console.WriteLine();
+
+Console.Write("Berapa jumlah buku tulis yang anda beli? ");
+int jumbukutulis = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int hargabukutulis = 7000;
+
+if (jumbukutulis >= 5 && jumbukutulis < 15)
+{
+    Console.WriteLine("Selamat! anda mendapat diskon sebesar 10%");
+    int harga = jumbukutulis * hargabukutulis * 10 / 100;
+    Console.WriteLine($"Harga yang harus anda bayar adalah = RP{harga}");
+}
+
+else if (jumbukutulis >= 15)
+{
+    Console.WriteLine("Selamat! anda mendapat diskon sebesar 15%");
+    int harga = jumbukutulis * hargabukutulis * 15 / 100;
+    Console.WriteLine($"Harga yang harus anda bayar adalah = Rp{harga}");
+}
+
+else
+{
+    int harga = jumbukutulis * hargabukutulis;
+    Console.WriteLine($"Harga yang harus anda bayar adalah = RP{harga}");
+}
+
+Console.WriteLine();
+
+//===============================================================================================================================================
---
+++ RM_Hari_1/RM_Hari_1/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("==================================================");
+Console.WriteLine("               Pemesanan Perabotan                ");
+Console.WriteLine("              --Semua Serba Rp25k--               ");
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine();
+
+int hargabarang = 25000;
+
+Console.Write("Masukkan nama barang = ");
+string? namabarang = Console.ReadLine();
+
+Console.Write("Berapa jumlah barang = ");
+int jumlahbarang = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Nominal uang yang anda bayar = Rp");
+int jumlahuang = Convert.ToInt32(Console.ReadLine());
+
+int totalbayar = jumlahbarang * hargabarang;
+int kembalian = jumlahuang - totalbayar;
+
+if(jumlahuang >= totalbayar)
+{
+    Console.WriteLine();
+    Console.WriteLine("--------------------------------------------------");
+    Console.WriteLine("                  Data Pemesanan                  ");
+    Console.WriteLine();
+
+    Console.WriteLine($"Barang yang dipilih         = {namabarang}");
+    Console.WriteLine($"Jumlah barang               = {jumlahbarang}");
+    Console.WriteLine($"Nominal uang yang dibayar   = Rp{jumlahuang}");
+    Console.WriteLine($"Nominal uang kembalian      = Rp{kembalian}");
+
+    Console.WriteLine();
+    Console.WriteLine("==================================================");
+}
+
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("--------------------------------------------------");
+    Console.WriteLine();
+    Console.WriteLine("Maaf, uang anda kurang untuk pembelian barang ini!");
+    Console.WriteLine($"Karena uang anda hanya sebanyak Rp{jumlahuang}, nominal yang seharusnya dibayar Rp{totalbayar}");
+    Console.WriteLine();
+    Console.WriteLine("==================================================");
+}
